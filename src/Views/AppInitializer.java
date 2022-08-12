f2dfe34f0a21bcbd135cfdf0300f36e8d138382c
Kaveesha Himasanka <kaveee143@gmail.com>
2022-08-12 17:24:03 +0530

    create login view

---
+++ src/Views/AppInitializer.java
@@ -0,0 +1,33 @@
+package Views;/**
+ * @author : Kaveesha Himasanka
+ * @package : Views
+ * @project : chatApp
+ * Kaveesha Himasanka
+ * 2022-Aug
+ * @since : 0.1.0
+ **/
+
+import com.sun.deploy.uitoolkit.impl.fx.ui.FXAboutDialog;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("EnterChat.fxml")));
+        primaryStage.setTitle("Tech Chat");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
---
+++ src/Views/EnterChatController.java
@@ -0,0 +1,12 @@
+package Views;
+
+/**
+ * @author : Kaveesha Himasanka
+ * @package : Views
+ * @project : chatApp
+ * Kaveesha Himasanka
+ * 2022-Aug
+ * @since : 0.1.0
+ **/
+public class EnterChatController {
+}
