d55855eaeb5f20cff3d0762d0e70ba322734827e
Kaveesha Himasanka <kaveee143@gmail.com>
2022-08-16 15:43:09 +0530

    set group chat

---
+++ src/GroupChat/Client/Main.java
@@ -0,0 +1,41 @@
+package GroupChat.Client;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    private static BridgeSocket bridgeSocket;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        bridgeSocket = new BridgeSocket("localhost", 8189);
+
+        if (bridgeSocket.connect().equals("fail"))
+            return;
+
+        PopUpDialog popUpDialog = new PopUpDialog(primaryStage, bridgeSocket);
+
+        String clientName = popUpDialog.getName();
+
+        MainDialog mainDialog = new MainDialog(clientName, bridgeSocket);
+
+        primaryStage.setTitle("Chat App");
+        primaryStage.setScene(new Scene(mainDialog.getBorderPane(), 270, 275));
+        primaryStage.show();
+        popUpDialog.show();
+
+        new SocketListener(bridgeSocket, mainDialog.getItems());
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        launch(args);
+        bridgeSocket.getSocket().close();
+    }
+}
---
+++ src/GroupChat/Server/Q.java
@@ -0,0 +1,22 @@
+package GroupChat.Server;
+
+public class Q {
+
+    String n;
+
+    synchronized String get(){
+        try{
+            wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return n;
+    }
+
+    synchronized void put(String n){
+
+        this.n = n;
+        notify();
+    }
+
+}
